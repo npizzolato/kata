87b695fc2859c0b432e7de5be5f388bb56986ef9
Nick <npizzolato@gmail.com>
2014-05-04 11:17:10 -0700

    Removed useless test

--- string_calculator/strcalc_tests.py
+++ string_calculator/strcalc_tests.py
@@ -11,9 +11,6 @@ class StringCalculatorTests(unittest.TestCase):
 	def test_add_one_number_returns_number(self):
 		self.assertEqual(1, self.sc.add("1"))
 
-	def test_add_two_numbers(self):
-		self.assertEqual(3, self.sc.add("1,2"))
-
 	def test_add_many_numbers(self):
 		self.assertEqual(10, self.sc.add("1,2,3,4"))
 
