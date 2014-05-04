a8f3cef1fe2545c140a7848c4dedc7d9f6cee8e9
Nick <npizzolato@gmail.com>
2014-05-04 11:01:48 -0700

    Test for multiple numbers

--- string_calculator/strcalc_tests.py
+++ string_calculator/strcalc_tests.py
@@ -2,17 +2,20 @@ import unittest
 from strcalc import StringCalculator
 
 class StringCalculatorTests(unittest.TestCase):
+	def setUp(self):
+		self.sc = StringCalculator()
+
 	def test_add_returns_0_for_empty_string(self):
-		sc = StringCalculator()
-		self.assertEqual(0, sc.add(""))
+		self.assertEqual(0, self.sc.add(""))
 
 	def test_add_one_number_returns_number(self):
-		sc = StringCalculator()
-		self.assertEqual(1, sc.add("1"))
+		self.assertEqual(1, self.sc.add("1"))
 
 	def test_add_two_numbers(self):
-		sc = StringCalculator()
-		self.assertEqual(3, sc.add("1,2"))
+		self.assertEqual(3, self.sc.add("1,2"))
+
+	def test_add_many_numbers(self):
+		self.assertEqual(10, self.sc.add("1,2,3,4"))
 
 if __name__ == "__main__":
 	unittest.main()
